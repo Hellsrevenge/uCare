0166436b5677c48ee34bc5f4c60145f798b0a037
Hellsrevenge <ivark26@gmail.com>
2019-01-31 11:42:28 -0800

    edit seeds

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const axios = require("axios");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18Populater";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/ucare";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
@@ -17,10 +17,10 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
-    app.get("api/patients/:id", function(req, res) {
+    app.get("/api/patients/:id", function(req, res) {
         db.Patient.findOne({ _id: req.params.id })
-            .populate("contact")
-            .populate("appointment")
+            .populate("contacts")
+            .populate("appointments")
             .then(function(dbPatient) {
                 res.json(dbPatient);
             })
@@ -29,3 +29,6 @@ if (process.env.NODE_ENV === "production") {
             });
     });
 
+app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
