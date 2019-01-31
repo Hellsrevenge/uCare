c35dd0711b3d62383e65f6e5e0b0987fb5939c19
Hellsrevenge <ivark26@gmail.com>
2019-01-30 22:22:52 -0800

    add models

---
+++ models/appointment.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Patient = require("./patient");
+const appointmentSchema = new Schema({
+    subject: {type: String},
+    date: {type: String},
+    status: {type: Number},
+    patient: {
+        type: Schema.Types.ObjectId,
+        ref: "Patient"
+    }
+});
+
+const Appointment = mongoose.model("Appointment", appointmentSchema);
+
+module.exports = Appointment;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    Patient: require("./patient"),
+    Appointment: require("./appointment"),
+    Contact: require("./contact"),
+};
