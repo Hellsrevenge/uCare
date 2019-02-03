fa5e4a74ad671caeb82bd70e69193c79fe5ae3d1
amandamurillo <amandasmurillo@gmail.com>
2019-02-02 16:04:24 -0800

    adding beginning of routes, still need to add more

--- client/src/pages/DoctorProfile/DoctorProfile.js
+++ client/src/pages/DoctorProfile/DoctorProfile.js
@@ -1,6 +1,23 @@
 import React from "react";
 import "./DoctorProfile.css";
 
+class Books extends Component {
+    state = {
+      
+    };
+  
+    componentDidMount() {
+      this.loadBooks();
+    }
+  
+    loadBooks = () => {
+      API.getBooks()
+        .then(res => this.setState({ books: res.data }))
+        .catch(err => console.log(err));
+    };
+
+
+
 function DoctorProfile(props) {
     return (
         <div>Hello</div>
---
+++ client/src/utils/API.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export default {
+
+  //HOME PAGE
+
+  //save new appointment to database
+  saveAppt: function(apptData, id) {
+    return axios.post("/api/patients/" + id, apptData);
+  },
+  // Gets list all of the patients
+  getPatients: function() {
+    return axios.get("/api/patients");
+  },
+  // Gets a patient file
+  getPatient: function(id) {
+    return axios.get("/api/patients/" + id);
+  },
+   // returns a search
+   searchFile: function(search) {
+    return axios.get("/api/search/");
+  }
+
+};
