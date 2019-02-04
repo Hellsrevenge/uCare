dd09ef6d69060b2ad573d27bb67169fe33c7882c
Hellsrevenge <ivark26@gmail.com>
2019-02-04 11:32:08 -0800

    react authentication

--- client/src/components/NavHome/NavHome.js
+++ client/src/components/NavHome/NavHome.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, {Component} from "react";
 import FormLogin from "../FormLogin/FormLogin";
+import AuthHelperMethods from '../../components/AuthHelperMethods';
 
+class NavHome extends Component {
+    Auth = new AuthHelperMethods();
 
-function NavHome() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a className="navbar-brand" href="/">
-          UCare
-        </a>
-      <FormLogin />
-      </nav>
-    );
-  };
+    handleLogout = () => {
+        this.Auth.logout();
+        this.props.history.replace('/');
+    };
+
+    renderLoginForm() {
+        if (this.Auth.loggedIn()) {
+            return (
+                <div className="right">
+                    <a href="/" onClick={this.handleLogout}>Logout</a>
+                </div>
+            )
+        } else {
+            return (
+                <FormLogin/>
+            )
+        }
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <a className="navbar-brand" href="/">
+                    UCare
+                </a>
+                {this.renderLoginForm()}
+            </nav>
+        );
+    }
+};
 
 export default NavHome;
