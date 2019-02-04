fbe534ae4a84d26ee3216d7f9e6ece7c52cb0c44
amandamurillo <amandasmurillo@gmail.com>
2019-02-03 18:20:11 -0800

    added new appointment page, still needs funcitonality

--- client/src/components/NavDoctor/NavDoctor.js
+++ client/src/components/NavDoctor/NavDoctor.js
@@ -1,14 +1,11 @@
 import React from "react";
-import drhouse from './drhouse.jpeg'; // Tell Webpack this JS file uses this image
 
 
 
 
 function NavDoctor() {
     return (
-
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-<img src={drhouse} alt="Doctor Picture" />
+<nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
