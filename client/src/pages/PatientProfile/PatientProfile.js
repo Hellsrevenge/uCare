77bef3725b40ef628f2ac102a8de4fbfb188baa3
Hellsrevenge <ivark26@gmail.com>
2019-02-04 23:47:43 -0800

    Add appointments to patient profile

--- client/src/pages/PatientProfile/PatientProfile.js
+++ client/src/pages/PatientProfile/PatientProfile.js
@@ -1,17 +1,47 @@
 import React, { Component} from "react";
 import NavHome from "../../components/NavHome/NavHome";
 import {Container} from "../../components/Grid/Grid";
-
-import AuthHelperMethods from '../../components/AuthHelperMethods';
 import withAuth from '../../components/withAuth';
 
 class PatientProfile extends Component {
-    Auth = new AuthHelperMethods();
+    constructor(props) {
+        super(props);
+        this.state = {
+            appointments: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("/api/appointments/")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({appointments: data});
+            });
+    }
 
     render() {
         return (
             <Container fluid>
                 <NavHome />
+                <div>
+                    <h3>Appointments</h3>
+                    {
+                        this.state.appointments ? (
+                            this.state.appointments.map((item, index) => {
+                                return (
+                                    <div key={index}>
+                                        {item.date} -
+                                        {item.subject} -
+                                        {item.status} -
+                                        {item.duration} -
+                                        <a href={item.skypeUrl}>{item.skypeUrl}</a> -
+                                        {item.DoctorId} -
+                                    </div>
+                                )
+                            })
+                        ) : null
+                    }
+                </div>
             </Container>
         )
     }
