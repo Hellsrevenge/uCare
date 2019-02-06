d24c2704e25e276b7db305819d411a5396c2f204
amandamurillo <amandasmurillo@gmail.com>
2019-02-06 00:31:56 -0800

    patient profile css

--- client/src/pages/PatientProfile/PatientProfile.js
+++ client/src/pages/PatientProfile/PatientProfile.js
@@ -3,6 +3,8 @@ import NavPatient from "../../components/NavPatient/NavPatient";
 import {Container, Row, Col} from "../../components/Grid/Grid";
 import withAuth from '../../components/withAuth';
 import Card from "../../components/Card/Card";
+import insimage from "./ins2.png";
+
 
 const oldmeds = {
     background: "#fcfc9c"
@@ -10,6 +12,12 @@ const oldmeds = {
   
 const currentmeds = {
     background: "#bdfcb3"
+  }
+
+  const cardImage = {
+    width: "300px",
+    height: "275px"
+
   }
 
 class PatientProfile extends Component {
@@ -40,9 +48,8 @@ class PatientProfile extends Component {
             <Row>
 
                 <Col size="6">
-                <Card>
-                <div>
-                    <h3>Appointments</h3>
+                <Card heading= {"Appointments"}>
+                
                     {
                         this.state.appointments ? (
                             this.state.appointments.map((item, index) => {
@@ -59,120 +66,75 @@ class PatientProfile extends Component {
                             })
                         ) : null
                     }
-                </div>
+                
                 </Card>
                 </Col>
 
                 <Col size="6">
-                <Card>
-                <div>
-                    <h3>Prescription Medications</h3>
-                </div>
-                <table className="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Name</th>
-      <th scope="col">Dosage</th>
-      <th scope="col">Reported Side Effects</th>
-    </tr>
-  </thead>
-  <tbody>
-<tr style={currentmeds}>
-      <th scope="row">1</th>
-      <td>Acetaminophen</td>
-      <td>800mg</td>
-      <td>N/A</td>
-    </tr>
-    <tr style={currentmeds}>
-      <th scope="row">2</th>
-      <td>Dextromethorphan</td>
-      <td>500 mg/m²</td>
-      <td>Drowsiness, vomitting</td>
-    </tr>
-    <tr style={oldmeds}>
-      <th scope="row">3</th>
-      <td>Loratadine </td>
-      <td>250 mg/m²</td>
-      <td>Upset stomach</td>
-    </tr>
-  </tbody>
-</table>
+                <Card heading= {"Prescription Medications"}>
+
+            <table className="table table-hover">
+              <thead>
+                 <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Dosage</th>
+                    <th scope="col">Reported Side Effects</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr style={currentmeds}>
+                    <th scope="row">1</th>
+                    <td>Acetaminophen</td>
+                    <td>800mg</td>
+                    <td>N/A</td>
+                </tr>
+                <tr style={currentmeds}>
+                    <th scope="row">2</th>
+                    <td>Dextromethorphan</td>
+                    <td>500 mg/m²</td>
+                    <td>Drowsiness, vomitting</td>
+                </tr>
+                <tr style={oldmeds}>
+                    <th scope="row">3</th>
+                    <td>Loratadine </td>
+                    <td>250 mg/m²</td>
+                    <td>Upset stomach</td>
+                </tr>
+              </tbody>
+            </table>
+                </Card>
+                </Col>
+
+                <Col size="6">
+                <Card heading= {"Insurance & Billing"}>
+                <img id="insphoto" src={insimage} style={cardImage} alt ="insurance"/>
+
+                </Card>
+                </Col>
+
+                <Col size="6">
+                <Card heading= {"Other Data"}>
+                    {
+                        this.state.appointments ? (
+                            this.state.appointments.map((item, index) => {
+                                return (
+                                    <div key={index}>
+                                        {item.date} -
+                                        {item.subject} -
+                                        {item.status} -
+                                        {item.duration} -
+                                        <a href={item.skypeUrl}>{item.skypeUrl}</a> -
+                                        {item.DoctorId} -
+                                    </div>
+                                )
+                            })
+                        ) : null
+                    }
                 </Card>
                 </Col>
             </Row>
-            
-            <Row>
-
-                <Col size="6">
-                <Card>
-                <div>
-                    <h3>Other Data</h3>
-                    {
-                        this.state.appointments ? (
-                            this.state.appointments.map((item, index) => {
-                                return (
-                                    <div key={index}>
-                                        {item.date} -
-                                        {item.subject} -
-                                        {item.status} -
-                                        {item.duration} -
-                                        <a href={item.skypeUrl}>{item.skypeUrl}</a> -
-                                        {item.DoctorId} -
-                                    </div>
-                                )
-                            })
-                        ) : null
-                    }
-                </div>
-                </Card>
-                </Col>
-
-                <Col size="6">
-                <Card>
-                <div>
-                    <h3>Appointments</h3>
-                    {
-                        this.state.appointments ? (
-                            this.state.appointments.map((item, index) => {
-                                return (
-                                    <div key={index}>
-                                        {item.date} -
-                                        {item.subject} -
-                                        {item.status} -
-                                        {item.duration} -
-                                        <a href={item.skypeUrl}>{item.skypeUrl}</a> -
-                                        {item.DoctorId} -
-                                    </div>
-                                )
-                            })
-                        ) : null
-                    }
-                </div>
-                </Card>
-                </Col>
-            </Row>
-{/* <Card>
-                <div>
-                    <h3>Appointments</h3>
-                    {
-                        this.state.appointments ? (
-                            this.state.appointments.map((item, index) => {
-                                return (
-                                    <div key={index}>
-                                        {item.date} -
-                                        {item.subject} -
-                                        {item.status} -
-                                        {item.duration} -
-                                        <a href={item.skypeUrl}>{item.skypeUrl}</a> -
-                                        {item.DoctorId} -
-                                    </div>
-                                )
-                            })
-                        ) : null
-                    }
-                </div>
-                </Card> */}
 
             </Container>
             </div>
