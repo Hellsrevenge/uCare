4b15d6942b2c35b16815365fa3caac118d22e2fe
Hellsrevenge <ivark26@gmail.com>
2019-02-07 00:15:07 -0800

    heroku 2

---
+++ client/build/precache-manifest.b2f0e95910eb5c5ce2b0a3e00d17fd33.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d50c7e336f38b4d019b8",
+    "url": "/static/css/main.2502470f.chunk.css"
+  },
+  {
+    "revision": "d50c7e336f38b4d019b8",
+    "url": "/static/js/main.d50c7e33.chunk.js"
+  },
+  {
+    "revision": "68da7d1b15426c338569",
+    "url": "/static/css/1.6ab0d7fe.chunk.css"
+  },
+  {
+    "revision": "68da7d1b15426c338569",
+    "url": "/static/js/1.68da7d1b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8e333231231e7687cdf6748cb095b415",
+    "url": "/static/media/logo.8e333231.png"
+  },
+  {
+    "revision": "5c2cb71938ec87c4104a980410828375",
+    "url": "/static/media/case3.5c2cb719.png"
+  },
+  {
+    "revision": "4b5b9f81cd3590f8b2c0ca16039ff49f",
+    "url": "/static/media/ins2.4b5b9f81.png"
+  },
+  {
+    "revision": "03cb270fcbc5bbbf9c7536bdbb8e5d54",
+    "url": "/index.html"
+  }
+];
