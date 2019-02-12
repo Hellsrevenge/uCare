1cdfab351b74c6320b8ca9cbb189550b1ce520d3
Hellsrevenge <ivark26@gmail.com>
2019-02-11 23:48:18 -0800

    Update server.js

--- server.js
+++ server.js
@@ -8,17 +8,22 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(__dirname));
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
+    app.get("/*", function(req, res) {
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
+} else {
+    app.use(express.static(path.join(__dirname, '/client/public')));
+    app.get("/*", function(req, res) {
+        res.sendFile(path.join(__dirname, "./client/public/index.html"));
+    });
 }
 
 require("./routes/patientRoutes")(app);
 require("./routes/appointmentRoutes")(app);
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"))
-});
 
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
